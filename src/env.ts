6d0bb99faa7d982e1f1df9c48b006ad7696617bc
artemushkaaa <shamaich007@gmail.com>
2024-09-12 16:37:01 +0300

    another changes

--- src/env.ts
+++ src/env.ts
@@ -15,7 +15,6 @@ export const FACEBOOK_VERIFICATION = process.env.FACEBOOK_VERIFICATION || ''
 
 export const REVALIDATE_SECRET = process.env.REVALIDATE_SECRET || 'hello'
 
-
 export const GOOGLE_API_TOKEN = 'AIzaSyABxgtOyM1xYumVno-MHCwykwrotVQ1ESs'
 export const ZONES_API_URL =
 	process.env.ZONES_API_URL || 'https://hellominsk5.cartrek.online/api/zones'
